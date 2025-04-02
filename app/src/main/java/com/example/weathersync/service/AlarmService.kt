538e2996b60cbaf2ee5051b8fc78114e4bed03a0
uFayad <youssefadelfayad@gmail.com>
2025-04-02 03:19:58 +0200

    Fix: Update Map API and Improved Weather Alert Service and UI

--- app/src/main/java/com/example/weathersync/service/AlarmService.kt
+++ app/src/main/java/com/example/weathersync/service/AlarmService.kt
@@ -12,8 +12,11 @@ import android.os.*
 import android.speech.tts.TextToSpeech
 import android.speech.tts.UtteranceProgressListener
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.annotation.RequiresPermission
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.example.weathersync.MainActivity
 import com.example.weathersync.R
 import com.example.weathersync.utils.WeatherUtils
 import java.util.*
@@ -21,28 +24,35 @@ import java.util.*
 class AlarmService : Service(), TextToSpeech.OnInitListener {
     private var tts: TextToSpeech? = null
     private val channelId = "weather_alarms_channel"
+    private val channelName = "Weather Alarms"
+    private val notificationId = 1
     private var isTtsInitialized = false
     private var ringtone: Ringtone? = null
     private var alarmManager: AlarmManager? = null
     private var message: String? = null
     private var isPlayingSound = false
 
-    @SuppressLint("ForegroundServiceType")
     override fun onCreate() {
         super.onCreate()
-
+        createNotificationChannel()
         tts = TextToSpeech(this, this)
-
-        val notification = createNotification()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            startForeground(1, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK)
-        } else {
-            startForeground(1, notification)
-        }
     }
 
+    @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
+    @SuppressLint("ForegroundServiceType")
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.d("AlarmService", "onStartCommand triggered")
+
+        val initialNotification = createNotification("Weather Alert", "Weather alert running")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            startForeground(
+                notificationId,
+                initialNotification,
+                ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK or ServiceInfo.FOREGROUND_SERVICE_TYPE_MICROPHONE
+            )
+        }  else {
+            startForeground(notificationId, initialNotification)
+        }
+
         alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
 
         val temperature = intent?.getDoubleExtra("temperature", 0.0)
@@ -51,8 +61,12 @@ class AlarmService : Service(), TextToSpeech.OnInitListener {
         val currentTemperatureUnit = intent?.getStringExtra("currentTemperatureUnit")
 
         if (temperature != null && description != null && currentTemperatureUnit != null) {
-            val temp = WeatherUtils.getFormattedTemperature(temperature, applicationContext)
-            message = "Weather is $description,\nTemperature is ${temp} °$currentTemperatureUnit,\nHumidity: $humidity%."
+            val temp = WeatherUtils.getFormattedTemperature(temperature, this)
+            message = getString(R.string.weather_alert)+
+                    getString(R.string.temperature_is, description, temp) + "$currentTemperatureUnit,"+
+                    getString(R.string.humidity)+"$humidity%"
+
+            updateNotification(getString(R.string.weather_alert), message ?: getString(R.string.weather_alert_running))
 
             playNotificationSound()
 
@@ -66,15 +80,46 @@ class AlarmService : Service(), TextToSpeech.OnInitListener {
         return START_STICKY
     }
 
-    private fun createNotification(): Notification {
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                description = "Weather alert notifications"
+            }
+
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+
+
+    private fun createNotification(title: String, text: String): Notification {
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
         return NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.drawable.app_logo)
-            .setContentTitle("Weather Alert")
-            .setContentText(message ?: "Weather alert running")
+            .setContentTitle(title)
+            .setContentText(text)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setCategory(NotificationCompat.CATEGORY_ALARM)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setContentIntent(pendingIntent)
             .setAutoCancel(true)
             .setOngoing(true)
             .build()
+    }
+
+    @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
+    private fun updateNotification(title: String, text: String) {
+        val notification = createNotification(title, text)
+        NotificationManagerCompat.from(this).notify(notificationId, notification)
     }
 
     private fun playNotificationSound() {
@@ -118,7 +163,9 @@ class AlarmService : Service(), TextToSpeech.OnInitListener {
 
     override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS) {
-            val result = tts?.setLanguage(Locale.getDefault())
+            val currentLocale = Locale.getDefault()
+            val isArabic = currentLocale.language == "ar"
+            val result = tts?.setLanguage(if (isArabic) Locale("ar") else currentLocale)
 
             if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                 return
@@ -133,12 +180,17 @@ class AlarmService : Service(), TextToSpeech.OnInitListener {
     }
 
     private fun stopForegroundService() {
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
         stopForeground(STOP_FOREGROUND_REMOVE)
         val notification = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.drawable.app_logo)
             .setContentTitle("Weather Alert")
             .setStyle(NotificationCompat.BigTextStyle().bigText(message ?: "Weather alert dismissed"))
             .setPriority(NotificationCompat.PRIORITY_MIN)
+            .setContentIntent(pendingIntent)
             .setSilent(true)
             .setAutoCancel(true)
             .build()
